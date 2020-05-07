985c3ce1fea2117a74bfeb5541e5e31e5381e5b5
Greg <63728373+fominykhgreg@users.noreply.github.com>
2020-05-07 22:59:08 +0300

    Add files via upload

---
+++ PauseEsc.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseEsc : MonoBehaviour {
+	private bool paused = false;
+	public GameObject panel;
+
+	public static bool pause = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (!paused) {
+				Time.timeScale = 0;
+				paused = true;
+				panel.SetActive (true);
+				AudioListener.pause = true;
+				
+			} else {
+				Time.timeScale = 1;
+				paused = false;
+				panel.SetActive (false);
+				AudioListener.pause = false;
+		
+			}
+		}
+		
+	}
+}
---
+++ button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip najim;
+
+    public void Sound()
+    {
+        myFx.PlayOneShot(najim);
+    }
+    // Start is called before the first frame update
+   
+}
